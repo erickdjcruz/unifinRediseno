7f9a3101434bb50f1ec5f84ae9cc26ba5eb019dd
erickdjcruz <erick.cruz@tactos.com.mx>
2025-01-21 19:13:49 -0600

    Ticket#33-21012025-dynamics365-clientes
    Api_Custom_TelefonoAudit-nuevo api de consulta
    accounts_dynamics365.php-defincion LH deshabilitada

--- custom/clients/base/api/getTelsClienteAudit.php
+++ custom/clients/base/api/getTelsClienteAudit.php
@@ -37,57 +37,107 @@ class getTelsClienteAudit extends SugarApi
         
         $idcliente = $args['idcliente'];
         $GLOBALS['log']->fatal('idcliente.'.$idcliente);
+        $list_idtels = [];
+        $list_parentid = [];
         $response = [];
 
-        $sql = "SELECT 
-            telaudit.date_created as fecha_modificacion, 
-            telaudit.created_by as id_usuario,
-            u.user_name as usuario , 
-            telaudit.parent_id as idtelefono, 
-            telaudit.field_name as campo_modificacion, 
-            telaudit.before_value_string as valor_previo, 
-            telaudit.after_value_string as valor_posterior,
-            audit_events.type as evento, 
-            audit_events.source
-        FROM tel_telefonos_audit telaudit 
-        INNER JOIN audit_events ON telaudit.event_id = audit_events.id
-        LEFT JOIN users u ON telaudit.created_by = u.id
-        WHERE telaudit.field_name = 'telefono' AND
-        telaudit.parent_id in (SELECT tel.id
+        $sql_idtels = "SELECT tel.id
         FROM tel_telefonos AS tel INNER JOIN accounts_tel_telefonos_1_c as acctel 
         ON tel.id = acctel.accounts_tel_telefonos_1tel_telefonos_idb
         WHERE acctel.accounts_tel_telefonos_1accounts_ida = '$idcliente' AND 
-        tel.deleted = 0 AND acctel.deleted = 0)
-            order by telaudit.date_created ASC";
+        tel.deleted = 0 AND acctel.deleted = 0";
+        $GLOBALS['log']->fatal("sql_idtels: " . $sql_idtels );
 
-        //$GLOBALS['log']->fatal("sql: " . $sql );
-        try{
-            $results = $db->query($sql); // Ejecutar la consulta
+        $results = $db->query($sql_idtels);
+        if( $results->num_rows > 0 ){
 
-            // Iterar por los resultados y almacenarlos en el arreglo
             while ($row = $db->fetchByAssoc($results)) {
-                $response[] = [
-                    'fecha_modificacion' => $row['fecha_modificacion'],
-                    'id_usuario' => $row['id_usuario'],
-                    'usuario' => $row['usuario'],
-                    'idtelefono' => $row['idtelefono'],
-                    'campo_modificacion' => $row['campo_modificacion'],
-                    'valor_previo' => $row['valor_previo'],
-                    'valor_posterior' => $row['valor_posterior'],
-                    'evento' => $row['evento'],
-                    'source' => json_decode($row['source']),
-                ];
+                array_push($list_idtels, $row['id'] );
             }
+
+            $valorestels = array_map(function($valor) {
+                return "'" . addslashes($valor) . "'";
+            }, $list_idtels);
             
-            // Registrar en el log el número de resultados obtenidos
-            $GLOBALS['log']->fatal("Número de resultados: " . count($response));
-        } catch (Exception $ex) {
-            $GLOBALS['log']->fatal("Exception " . $ex);
-            $estado = 400;
-            $response = [
-                'error' => true,
-                'message' => 'Ocurrió un error al procesar la consulta. Por favor, revisa los logs para más detalles.'
-            ];
+            // Convertir a string
+            $valString1 = implode(',', $valorestels);
+
+            $sql_idparents = "SELECT telaudit.parent_id ,telaudit.field_name as campo_modificacion
+            FROM tel_telefonos_audit telaudit
+            WHERE telaudit.field_name = 'telefono' AND
+            telaudit.parent_id in ($valString1)
+            order by telaudit.date_created";
+            $GLOBALS['log']->fatal("sql_idparents: " . $sql_idparents );
+
+            $results1 = $db->query($sql_idparents);
+
+            if( $results1->num_rows > 0 ){
+
+                while ($row = $db->fetchByAssoc($results1)) {
+                    array_push($list_parentid, $row['parent_id'] );
+                }
+
+                $valoresparents = array_map(function($valor) {
+                    return "'" . addslashes($valor) . "'";
+                }, $list_parentid);
+
+                // Convertir a string
+                $valString2 = implode(',', $valoresparents);
+
+                $sql = "SELECT * FROM (SELECT telaudit.date_created as fecha_modificacion, telaudit.created_by as id_usuario,
+                u.user_name as usuario , telaudit.parent_id as idtelefono, telaudit.field_name as campo_modificacion, 
+                telaudit.before_value_string as valor_previo, telaudit.after_value_string as valor_posterior,
+                audit_events.type as evento, audit_events.source
+                FROM tel_telefonos_audit telaudit 
+                INNER JOIN audit_events ON telaudit.event_id = audit_events.id
+                LEFT JOIN users u ON telaudit.created_by = u.id
+                WHERE telaudit.field_name = 'telefono' AND
+                telaudit.parent_id in ($valString2)
+                order by telaudit.date_created
+                ) as A , 
+                (SELECT u.user_name as usuario_plataforma , telaudit.field_name as campo_modificacion , 
+                telaudit.parent_id
+                FROM tel_telefonos_audit telaudit 
+                LEFT JOIN users u ON telaudit.created_by = u.id
+                WHERE telaudit.field_name = 'plataforma' AND  
+                telaudit.parent_id  in ($valString2)
+                order by telaudit.date_created
+                ) as B
+                WHERE A.idtelefono = B.parent_id";
+
+                $GLOBALS['log']->fatal("sql: " . $sql );
+                try{
+                    $resultsF = $db->query($sql); // Ejecutar la consulta
+
+                    if( $resultsF->num_rows > 0 ){
+                        // Iterar por los resultados y almacenarlos en el arreglo
+                        while ($row = $db->fetchByAssoc($resultsF)) {
+                            $response[] = [
+                                'fecha_modificacion' => $row['fecha_modificacion'],
+                                'id_usuario' => $row['id_usuario'],
+                                'usuario' => $row['usuario'],
+                                'plataforma' => $row['usuario_plataforma'],
+                                'idtelefono' => $row['idtelefono'],
+                                'campo_modificacion' => $row['campo_modificacion'],
+                                'valor_previo' => $row['valor_previo'],
+                                'valor_posterior' => $row['valor_posterior'],
+                                'evento' => $row['evento'],
+                                // 'source' => json_decode($row['source']),
+                            ];
+                        }
+                        
+                        // Registrar en el log el número de resultados obtenidos
+                        $GLOBALS['log']->fatal("Número de resultados: " . count($response));
+                    }
+                } catch (Exception $ex) {
+                    $GLOBALS['log']->fatal("Exception " . $ex);
+                    $estado = 400;
+                    $response = [
+                        'error' => true,
+                        'message' => 'Ocurrió un error al procesar la consulta. Por favor, revisa los logs para más detalles.'
+                    ];
+                }
+            }
         }
 
         return $response;
