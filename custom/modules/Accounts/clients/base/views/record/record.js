4b89a5aec88122c464d6827395c8fd16bcc2e26b
erickdjcruz <erick.cruz@tactos.com.mx>
2025-03-04 13:01:59 -0600

    validacion_uniclick_20250304
    Se comento la validación para el producto uniclick, en create, create-nodupecheck, record

--- custom/modules/Accounts/clients/base/views/record/record.js
+++ custom/modules/Accounts/clients/base/views/record/record.js
@@ -316,7 +316,7 @@
         this.model.addValidationTask('CreditAutoUP', _.bind(this.requeridosCAUP, this));
         this.model.addValidationTask('FleetUP', _.bind(this.requeridosFleetUP, this));
         this.model.addValidationTask('UniclickUP', _.bind(this.requeridosUniclickUP, this));
-        this.model.addValidationTask('UniclickCanal', _.bind(this.requeridosUniclickCanal, this));
+        //this.model.addValidationTask('UniclickCanal', _.bind(this.requeridosUniclickCanal, this));
         this.model.addValidationTask('tipo_proveedor_compras', _.bind(this.tipoProveedor, this));
         this.model.addValidationTask('AlertaCamposRequeridosUniclick', _.bind(this.validaReqUniclick, this));
         this.model.addValidationTask('validaReqPLDPropReal_CS', _.bind(this.validaPropRealCR, this));
