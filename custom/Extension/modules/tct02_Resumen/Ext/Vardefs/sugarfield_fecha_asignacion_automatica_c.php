d4d85f1814c3486036ca01b94dcf64fd071c9ae4
erickdjcruz <erick.cruz@tactos.com.mx>
2025-03-31 14:45:17 -0600

    Asignacion_automatica-estadistico - CP-4305-20250331
    nuevos campos para control de asignacion-fechas y asignado

---
+++ custom/Extension/modules/tct02_Resumen/Ext/Vardefs/sugarfield_fecha_asignacion_automatica_c.php
@@ -0,0 +1,9 @@
+<?php
+ // created: 2025-03-31 20:12:54
+$dictionary['tct02_Resumen']['fields']['fecha_asignacion_automatica_c']['labelValue']='fecha asignacion automatica';
+$dictionary['tct02_Resumen']['fields']['fecha_asignacion_automatica_c']['enforced']='';
+$dictionary['tct02_Resumen']['fields']['fecha_asignacion_automatica_c']['dependency']='';
+$dictionary['tct02_Resumen']['fields']['fecha_asignacion_automatica_c']['required_formula']='';
+$dictionary['tct02_Resumen']['fields']['fecha_asignacion_automatica_c']['readonly_formula']='';
+
+ ?>
