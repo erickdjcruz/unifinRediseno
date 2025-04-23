f5e2f8b83e98102a9498bfa050ef6b0e9ca446bf
IRVING DELGADO <irving.delgado@tactos.com.mx>
2025-04-23 09:59:14 -0600

    ID: CRM - Se agrega ajuste para quitar la dependencia de studio de detalle origen en PO - Requerimiento Alianzas Konnect - Cambios en CRM CP-4328

--- custom/Extension/modules/Prospects/Ext/Vardefs/sugarfield_detalle_origen_c.php
+++ custom/Extension/modules/Prospects/Ext/Vardefs/sugarfield_detalle_origen_c.php
@@ -1,100 +1,10 @@
 <?php
- // created: 2025-02-05 13:10:01
+ // created: 2025-04-19 14:44:00
 $dictionary['Prospect']['fields']['detalle_origen_c']['labelValue']='Detalle Origen';
 $dictionary['Prospect']['fields']['detalle_origen_c']['enforced']='';
-$dictionary['Prospect']['fields']['detalle_origen_c']['dependency']='';
 $dictionary['Prospect']['fields']['detalle_origen_c']['required_formula']='';
 $dictionary['Prospect']['fields']['detalle_origen_c']['readonly_formula']='';
 $dictionary['Prospect']['fields']['detalle_origen_c']['visibility_grid']=array (
-  'trigger' => 'origen_c',
-  'values' => 
-  array (
-    1 => 
-    array (
-      0 => '',
-      1 => '3',
-      2 => '9',
-      3 => '5',
-      4 => '11',
-      5 => '70',
-      6 => '71',
-      7 => '72',
-      8 => '73',
-      9 => '74',
-      10 => '75',
-      11 => '76',
-      12 => '77',
-      13 => '78',
-      14 => '79',
-      15 => '80',
-      16 => '81',
-      17 => '82',
-      18 => '83',
-      19 => '84',
-      20 => '85',
-      21 => '104',
-      22 => '105',
-      23 => '106',
-      24 => '107',
-      25 => '108',
-      26 => '109',
-      27 => '110',
-    ),
-    3 => 
-    array (
-    ),
-    6 => 
-    array (
-    ),
-    8 => 
-    array (
-    ),
-    9 => 
-    array (
-    ),
-    11 => 
-    array (
-    ),
-    12 => 
-    array (
-      0 => '',
-      1 => '12',
-      2 => '13',
-      3 => '114',
-    ),
-    13 => 
-    array (
-      0 => '63',
-      1 => '64',
-    ),
-    14 => 
-    array (
-      0 => '65',
-    ),
-    15 => 
-    array (
-      0 => '66',
-      1 => '67',
-      2 => '68',
-      3 => '69',
-    ),
-    17 => 
-    array (
-    ),
-    18 => 
-    array (
-    ),
-    19 => 
-    array (
-    ),
-    20 => 
-    array (
-      0 => '113',
-    ),
-    '' => 
-    array (
-    ),
-  ),
 );
 
  ?>
