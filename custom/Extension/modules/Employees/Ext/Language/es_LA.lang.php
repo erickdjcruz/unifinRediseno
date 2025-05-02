7b8629b8e83dd12bf5ae1a3324f199d7c2e72120
IRVING DELGADO <irving.delgado@tactos.com.mx>
2025-05-02 09:17:45 -0600

    ID: CRM - Ajuste - Se implementa permiso para poder capturar la dirección de Sepomex - Direcciones Sepomex - Ticket# 5-25072024 - CP-4240

--- custom/Extension/modules/Employees/Ext/Language/es_LA.lang.php
+++ custom/Extension/modules/Employees/Ext/Language/es_LA.lang.php
@@ -74,3 +74,4 @@ $mod_strings['LBL_CANCELAR_CASOS'] = 'Cancelar casos';
 $mod_strings['LBL_SEGUIMIENTO_BC'] = 'Seguimiento Buró de Crédito';
 $mod_strings['LBL_GESTION_UTILITY_TRAILERS_PO'] = 'Gestion Utility Trailers PO';
 $mod_strings['LBL_GESTION_TEAM_LEADERS_C'] = 'Gestión Team Leaders';
+$mod_strings['LBL_CAPTURA_DIRECCION_SEPOMEX'] = 'Captura Dirección Sepomex';
---
+++ custom/Extension/modules/Users/Ext/Vardefs/sugarfield_captura_direccion_sepomex_c.php
@@ -0,0 +1,8 @@
+<?php
+ // created: 2025-04-30 21:02:24
+$dictionary['User']['fields']['captura_direccion_sepomex_c']['labelValue']='Captura Dirección Sepomex';
+$dictionary['User']['fields']['captura_direccion_sepomex_c']['enforced']='';
+$dictionary['User']['fields']['captura_direccion_sepomex_c']['dependency']='';
+$dictionary['User']['fields']['captura_direccion_sepomex_c']['readonly_formula']='';
+
+ ?>
