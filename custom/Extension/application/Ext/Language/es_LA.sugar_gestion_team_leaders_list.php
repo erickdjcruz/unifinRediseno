d96542744d43a9cf68bd78bd2e0b496646372c07
IRVING DELGADO <irving.delgado@tactos.com.mx>
2025-06-04 00:09:23 -0600

    ID: CRM - Requerimiento Alianzas Reditus- Cambios en CRM

--- custom/Extension/application/Ext/Language/es_LA.sugar_gestion_team_leaders_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_gestion_team_leaders_list.php
@@ -1,7 +1,8 @@
 <?php
- // created: 2025-05-21 11:08:37
+ // created: 2025-06-03 21:59:02
 
 $app_list_strings['gestion_team_leaders_list']=array (
   'konnect' => 'Konnect',
   'vendors' => 'Vendors',
+  'reditus' => 'Reditus',
 );
