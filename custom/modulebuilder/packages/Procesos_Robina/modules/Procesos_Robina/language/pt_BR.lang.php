c3d01656e5c35069e167b87eb3429f23601fc3ab
IRVING DELGADO <irving.delgado@tactos.com.mx>
2025-06-09 16:02:55 -0600

    ID: CRM - Se agrega nuevo modulo de procesos Robina

---
+++ custom/modulebuilder/packages/Procesos_Robina/modules/Procesos_Robina/language/pt_BR.lang.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+$mod_strings = array (
+  'LBL_TEAM' => 'Equipes',
+  'LBL_TEAMS' => 'Equipes',
+  'LBL_TEAM_ID' => 'Id da Equipe',
+  'LBL_ASSIGNED_TO_ID' => 'ID do usuário atribuído',
+  'LBL_ASSIGNED_TO_NAME' => 'Atribuído a',
+  'LBL_TAGS_LINK' => 'Marcações',
+  'LBL_TAGS' => 'Marcações',
+  'LBL_ID' => 'ID',
+  'LBL_DATE_ENTERED' => 'Data de criação',
+  'LBL_DATE_MODIFIED' => 'Data da Modificação',
+  'LBL_MODIFIED' => 'Modificado Por',
+  'LBL_MODIFIED_ID' => 'Modificado Por Id',
+  'LBL_MODIFIED_NAME' => 'Modificado por nome',
+  'LBL_CREATED' => 'Criado Por',
+  'LBL_CREATED_ID' => 'Criado Por Id',
+  'LBL_DOC_OWNER' => 'Proprietário do documento',
+  'LBL_USER_FAVORITES' => 'Usuários Favoritos',
+  'LBL_DESCRIPTION' => 'Descrição',
+  'LBL_DELETED' => 'Excluído',
+  'LBL_NAME' => 'Nome',
+  'LBL_CREATED_USER' => 'Criado pelo usuário',
+  'LBL_MODIFIED_USER' => 'Modificado pelo usuário',
+  'LBL_LIST_NAME' => 'Nome',
+  'LBL_EDIT_BUTTON' => 'Editar',
+  'LBL_REMOVE' => 'Remover',
+  'LBL_EXPORT_MODIFIED_BY_NAME' => 'Modificado por nome',
+  'LBL_EXPORT_CREATED_BY_NAME' => 'Criado por nome',
+  'LBL_COMMENTLOG' => 'Comment Log',
+  'LBL_LIST_FORM_TITLE' => 'Procesos Robina Lista',
+  'LBL_MODULE_NAME' => 'Procesos Robina',
+  'LBL_MODULE_TITLE' => 'Procesos Robina',
+  'LBL_MODULE_NAME_SINGULAR' => 'Procesos Robina',
+  'LBL_HOMEPAGE_TITLE' => 'Minha Procesos Robina',
+  'LNK_NEW_RECORD' => 'Criar Procesos Robina',
+  'LNK_LIST' => 'Visualização Procesos Robina',
+  'LNK_IMPORT_PR_PROCESOS_ROBINA' => 'Importar Procesos Robina',
+  'LBL_SEARCH_FORM_TITLE' => 'Pesquisar Procesos Robina',
+  'LBL_HISTORY_SUBPANEL_TITLE' => 'Ver histórico',
+  'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Cadeia de atividades',
+  'LBL_PR_PROCESOS_ROBINA_SUBPANEL_TITLE' => 'Procesos Robina',
+  'LBL_NEW_FORM_TITLE' => 'Novo Procesos Robina',
+  'LNK_IMPORT_VCARD' => 'Importar Procesos Robina vCard',
+  'LBL_IMPORT' => 'Importar Procesos Robina',
+  'LBL_IMPORT_VCARDTEXT' => 'Automatically create a new Procesos Robina record by importing a vCard from your file system.',
+  'LBL_PR_PROCESOS_ROBINA_FOCUS_DRAWER_DASHBOARD' => 'Procesos Robina Panel de Enfoque',
+  'LBL_PR_PROCESOS_ROBINA_RECORD_DASHBOARD' => 'Procesos Robina Tablero de Registro',
+  'LBL_RFC' => 'RFC',
+  'LBL_TICKET' => 'Ticket',
+  'LBL_ESTATUS_PROCESADO' => 'Estatus procesado',
+  'LBL_ESTATUS_ROBINA' => 'Estatus robina',
+  'LBL_FECHA_EMISION' => 'Fecha emision',
+);
