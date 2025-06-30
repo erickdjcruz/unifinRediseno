0741c6a4070fdd4c60ede15e50ea4fdfeae5d4c5
IRVING DELGADO <irving.delgado@tactos.com.mx>
2025-06-30 17:34:31 -0600

    ID: CRM - Se agrega ajuste en la validacion de campo custom para Franquicia Vendors_ - CP-4332

--- custom/Extension/modules/Prospects/Ext/Vardefs/sugarfield_franquicia_c.php
+++ custom/Extension/modules/Prospects/Ext/Vardefs/sugarfield_franquicia_c.php
@@ -1,14 +1,14 @@
 <?php
- // created: 2025-06-30 10:54:00
-$dictionary['Prospect']['fields']['franquicia_c']['labelValue']='Franquicia';
-$dictionary['Prospect']['fields']['franquicia_c']['full_text_search']=array (
+// created: 2025-06-30 09:39:28
+$dictionary['Prospect']['fields']['franquicia_c']['labelValue'] = 'Franquicia';
+$dictionary['Prospect']['fields']['franquicia_c']['full_text_search'] = array(
   'enabled' => '0',
   'boost' => '1',
   'searchable' => false,
 );
-$dictionary['Prospect']['fields']['franquicia_c']['enforced']='';
-$dictionary['Prospect']['fields']['franquicia_c']['dependency']='';
-$dictionary['Prospect']['fields']['franquicia_c']['required_formula']='';
-$dictionary['Prospect']['fields']['franquicia_c']['readonly_formula']='';
+$dictionary['Prospect']['fields']['franquicia_c']['enforced'] = '';
+$dictionary['Prospect']['fields']['franquicia_c']['dependency'] = 'and(equal($origen_c,"12"),or(equal($detalle_origen_c,"12"),equal($detalle_origen_c,"13"),equal($detalle_origen_c,"114"),equal($detalle_origen_c,"115"),equal($detalle_origen_c,"117")))';
+$dictionary['Prospect']['fields']['franquicia_c']['required_formula'] = '';
+$dictionary['Prospect']['fields']['franquicia_c']['readonly_formula'] = '';
 
- ?>
+?>
