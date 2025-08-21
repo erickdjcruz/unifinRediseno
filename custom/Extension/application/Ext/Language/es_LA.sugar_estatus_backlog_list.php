efd3dda202f5f30e14fdf75ce10718077ba23bd2
IRVING DELGADO <irving.delgado@tactos.com.mx>
2025-08-21 16:45:11 -0600

    ID: CRM - Se acomoda valores de la lista desplegable de estatus backlog - CP-4351

--- custom/Extension/application/Ext/Language/es_LA.sugar_estatus_backlog_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_estatus_backlog_list.php
@@ -1,12 +1,11 @@
 <?php
- // created: 2025-08-21 14:36:11
+ // created: 2025-08-21 16:31:21
 
 $app_list_strings['estatus_backlog_list']=array (
-  '' => '',
-  1 => 'Inválida',
-  2 => 'Cancelada por vigencia',
-  3 => 'Bloqueada por expediente',
-  4 => 'Declinada',
-  5 => 'Solicitud de reactivación',
-  6 => 'Activa',
+  1 => 'Activa',
+  2 => 'Declinada',
+  3 => 'Inválida',
+  4 => 'Cancelada por vigencia',
+  5 => 'Bloqueada por expediente',
+  6 => 'Solicitud de reactivación',
 );
