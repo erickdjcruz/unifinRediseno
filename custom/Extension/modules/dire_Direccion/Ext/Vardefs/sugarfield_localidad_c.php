2baaa207016b19e3b61d6d4a873734219e6f8adc
erickDjesus <erick.cruz@tactos.com.mx>
2025-09-11 11:05:28 -0600

    buro_credito-20250911
    campo localidad

---
+++ custom/Extension/modules/dire_Direccion/Ext/Vardefs/sugarfield_localidad_c.php
@@ -0,0 +1,14 @@
+<?php
+ // created: 2025-09-11 16:38:40
+$dictionary['dire_Direccion']['fields']['localidad_c']['labelValue']='LBL_LOCALIDAD';
+$dictionary['dire_Direccion']['fields']['localidad_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['dire_Direccion']['fields']['localidad_c']['enforced']='';
+$dictionary['dire_Direccion']['fields']['localidad_c']['dependency']='';
+$dictionary['dire_Direccion']['fields']['localidad_c']['required_formula']='';
+$dictionary['dire_Direccion']['fields']['localidad_c']['readonly_formula']='';
+
+ ?>
